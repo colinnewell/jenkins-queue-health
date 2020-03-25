18e8a25f0f1352a8552c28125672e81b3350d3fc
Colin Newell <colin.newell@gmail.com>
2020-03-25 21:24:41 +0000

    Start to fill in code

--- jenkins.go
+++ jenkins.go
@@ -2,20 +2,28 @@ package jenkins_health
 
 import (
 	"fmt"
-	"net/http"
+
+	resty "github.com/go-resty/resty/v2"
 )
 
 type JenkinsAPI struct {
-	Client http.Client
+	Client     resty.Client
+	JenkinsURL string
 }
 
-func (jenkins *JenkinsAPI) Runs(jobName string) ([]int, error) {
+func (jenkins *JenkinsAPI) Runs(jobName string) ([]string, error) {
+	url := fmt.Sprintf("%s/job/%s/api/json?tree=builds[url]",
+		jenkins.JenkinsURL,
+		jobName,
+	)
+	r, err := jenkins.Client.R().Get(url)
+	if err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
 
-func (jenkins *JenkinsAPI) ConsoleLog(jobName string, build int) (string, error) {
-	// get a list of recent builds for the job
-	// grab the logs
-	fmt.Println("vim-go")
+func (jenkins *JenkinsAPI) ConsoleLog(jobName string, buildUrl string) (string, error) {
+	url := fmt.Sprintf("%s/logText/progressiveText?start=0", buildUrl)
 	return "", nil
 }
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package jenkins_health
 import (
 	"fmt"
 	"log"
+
+	resty "github.com/go-resty/resty/v2"
 )
 
 func main() {
@@ -10,7 +12,12 @@ func main() {
 	// job number, pass/fail, failure type, time to run, machine
 	// console log
 	// can I get timing info?  Mathew mentioned that was somewhere
-	j := &JenkinsAPI{}
+	client := resty.New()
+	client.SetBasicAuth("uadmin", "119f8713bc75a829dbc4df57170ed8f5a3")
+	j := &JenkinsAPI{
+		Client:     client,
+		JenkinsURL: "http://localhost:8080",
+	}
 
 	text, err := j.ConsoleLog("cvl-gerrit", 1003)
 	if err != nil {
