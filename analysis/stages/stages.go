2a725ea3906b0c603b54e46ea58a6fa792b0e719
Colin Newell <colin.newell@gmail.com>
2020-08-29 11:42:04 +0100

    Switch the way we detect there are no stages

--- analysis/stages/stages.go
+++ analysis/stages/stages.go
@@ -14,17 +14,18 @@ type Analyser struct {
 func (a *Analyser) AnalyseBuild(an *analysis.AnalysedBuild) error {
 	// rip through the log and turn it into stages
 	stages := strings.Split(an.ConsoleLog, "\r\n// [Pipeline] stage\r\n")
+	if len(stages) < 2 {
+		return nil
+	}
 	for _, stage := range stages {
 		name := ""
 		// find first
 		//[Pipeline] { (Test)
 		// then look for first and last timestamps
-		if name != "" {
-			an.Stages = append(an.Stages, analysis.BuildStage{
-				Name: name,
-				Log:  stage,
-			})
-		}
+		an.Stages = append(an.Stages, analysis.BuildStage{
+			Name: name,
+			Log:  stage,
+		})
 	}
 	return nil
 }
